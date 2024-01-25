8e7eded453b02ba69c2c71bd0bfa8a5949339720
SaldNevo <artemkorobov4119@gmail.com>
2024-01-25 18:50:14 +0300

    burger menu and 3 additional pages

---
+++ script.js
@@ -0,0 +1,14 @@
+const burger = document.querySelector('.burger')
+const navList = document.querySelector('.navigation__list')
+const wrapper = document.querySelector('.wrapper')
+const headerAbsolute = document.querySelector('.header')
+const headerWrapper = document.querySelector('.header__wrapper')
+const body = document.querySelector('body')
+
+burger.addEventListener('click' , () => {
+  navList.classList.toggle('active__list')
+  headerWrapper.classList.toggle('active__list')
+  wrapper.classList.toggle('wrapper__active')
+  headerAbsolute.classList.toggle('header__absolute')
+  body.classList.toggle('noscroll')
+})
