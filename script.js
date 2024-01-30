a508dd7e0b5ea9cbb30b67ea2f564441dc881685
SaldNevo <artemkorobov4119@gmail.com>
2024-01-30 23:07:46 +0300

    I finished the links and the adaptive, tried to make a slider

--- script.js
+++ script.js
@@ -5,10 +5,32 @@ const headerAbsolute = document.querySelector('.header')
 const headerWrapper = document.querySelector('.header__wrapper')
 const body = document.querySelector('body')
 
-burger.addEventListener('click' , () => {
-  navList.classList.toggle('active__list')
-  headerWrapper.classList.toggle('active__list')
-  wrapper.classList.toggle('wrapper__active')
-  headerAbsolute.classList.toggle('header__absolute')
-  body.classList.toggle('noscroll')
+burger.addEventListener('click', () => {
+	navList.classList.toggle('active__list')
+	headerWrapper.classList.toggle('active__list')
+	wrapper.classList.toggle('wrapper__active')
+	headerAbsolute.classList.toggle('header__absolute')
+	body.classList.toggle('noscroll')
 })
+
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
+
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+  slidesPerView: 2,
+})
+
+
+
