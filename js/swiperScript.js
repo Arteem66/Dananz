574b9c3c036213fd6572aaa7f95c17ad9a071595
SaldNevo <artemkorobov4119@gmail.com>
2024-02-02 00:20:45 +0300

    I finished swiper, I did everything I wanted to do

---
+++ js/swiperScript.js
@@ -0,0 +1,52 @@
+const swiperWrapper = document.querySelector('.swiper-wrapper')
+const pageWidth = document.documentElement.scrollWidth
+
+
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	direction: 'horizontal',
+	
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+	},
+	
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+	
+	breakpoints: {
+		1230: {
+			enabled: false,
+		},
+		1000: {
+			slidesPerView: 3,
+			enabled: true,
+		},
+		780: {
+			slidesPerView: 2,
+			enabled: true,
+		},
+		300: {
+			slidesPerView: 1,
+			enabled: true,
+		},
+	},
+})
+
+if (pageWidth <= 1230) {
+	swiperWrapper.classList.remove('designers__list')
+} else {
+	swiperWrapper.classList.add('designers__list')
+}
+
+window.addEventListener('resize', function () {
+	if (window.matchMedia('(min-width: 1230px)').matches) {
+		swiperWrapper.classList.add('designers__list')
+	} else {
+		swiperWrapper.classList.remove('designers__list')
+	}
+})
+
